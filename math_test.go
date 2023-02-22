8cee1255a1e3bb5cd0f7bd3b72d9cf5ff1d00be3
Rodrigo Mariano <rodspt@gmail.com>
2023-02-21 23:15:10 -0300

    workflow

--- math_test.go
+++ math_test.go
@@ -7,6 +7,6 @@ func TestSoma(t *testing.T) {
 	total := soma(15, 15)
 
 	if total != 30 {
-		t.Errorf("Resultado da some é inválidox:  Resultado: %d. Esperado: %d", total, 30)
+		t.Errorf("Resultado da some é inválido:  Resultado: %d. Esperado: %d", total, 30)
 	}
 }
